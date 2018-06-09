af346daa0cb85c169adbc1ece87b0b5bda64d7f2
EC2 Default User <ec2-user@ip-172-31-5-252.us-east-2.compute.internal>
2018-06-09 00:04:08 +0000

    Add acount activation

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,11 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
-  resources :users
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :users
+  resources :account_activations, only: [:edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
